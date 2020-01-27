4c83ee93b6954dc3be1946246b6208aa9c56992f
sumitra19jha <sumitra19jha@gmail.com>
2020-01-27 23:54:06 +0530

    pug  dynamic rendering

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const adminData = require('./admin');
+
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
