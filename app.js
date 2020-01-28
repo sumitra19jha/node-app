f72767623fe2cefe1e9f880b64a7b45c20617ca7
sumitra19jha <sumitra19jha@gmail.com>
2020-01-28 16:43:04 +0530

    Working with dynamic content by using pug, handlebafrs and ejs templeting engine.

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-//This setter is used for rendering dynamic content
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -17,8 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(404).render('404');
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 })
 
 app.listen(3000);
